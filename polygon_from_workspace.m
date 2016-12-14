e8d1b3db9ef12bf87e7d2445fa4af4b92c42d215
matejjeglic <matej.jeglic@gmail.com>
2016-12-14 23:42:36 +0100

    updated datapoint form picture. Added a correct function for polygon drawing.

--- polygon_from_workspace.m
+++ polygon_from_workspace.m
@@ -3,15 +3,18 @@ close all;
 
 load('Polygon_map')
 
-% figure
-figure('Position', [1550 100 40*35 20*35])
-hold on
+
 
 %% Choose colorMap
 % colorMap = colorMap_Original;
 colorMap = colorMap_Pastel;
 
 %% Draw polygon
+
+figure
+% figure('Position', [1550 100 40*35 20*35])
+hold on
+
 for i = 1:length(colorMap)
     bool = (colors == i-1);
     xDraw = xGrid(bool);
